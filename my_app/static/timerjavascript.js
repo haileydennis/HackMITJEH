d6bc1a3992ee59c69efe821393b25fa247ccae31
= <jesszhz@hotmail.com>
2020-09-20 06:10:38 -0400

    Added Javascript code for countdown timer -- untested

--- my_app/static/timerjavascript.js
+++ my_app/static/timerjavascript.js
@@ -1,82 +1,53 @@
-var startTimerButton = document.querySelector('.startTimer');
-var pauseTimerButton = document.querySelector('.pauseTimer');
-var timerDisplay = document.querySelector('.timer');
-var startTime;
-var updatedTime;
-var difference;
-var tInterval;
-var savedTime;
-var paused = 0;
-var running = 0;
-function startTimer(){
-  if(!running){
-    startTime = new Date().getTime();
-    tInterval = setInterval(getShowTime, 1);
-// change 1 to 1000 above to run script every second instead of every millisecond. one other change will be needed in the getShowTime() function below for this to work. see comment there.
+var countDownTime = -1;
+var seshOn = 0;
+var sBreakOn = 0;
+var lBreakOn = 0;
+var timerOn = 0;
+var prevState = -1;  // stores the last state the timer was in, denoting 0 - sesh, 1 - short break, 2 - long break 
 
-    paused = 0;
-    running = 1;
-    timerDisplay.style.background = "#FF0000";
-    timerDisplay.style.cursor = "auto";
-    timerDisplay.style.color = "yellow";
-    startTimerButton.classList.add('lighter');
-    pauseTimerButton.classList.remove('lighter');
-    startTimerButton.style.cursor = "auto";
-    pauseTimerButton.style.cursor = "pointer";
-  }
-}
-function pauseTimer(){
-  if (!difference){
-    // if timer never started, don't allow pause button to do anything
-  } else if (!paused) {
-    clearInterval(tInterval);
-    savedTime = difference;
-    paused = 1;
-    running = 0;
-    timerDisplay.style.background = "#A90000";
-    timerDisplay.style.color = "#690000";
-    timerDisplay.style.cursor = "pointer";
-    startTimerButton.classList.remove('lighter');
-    pauseTimerButton.classList.add('lighter');
-    startTimerButton.style.cursor = "pointer";
-    pauseTimerButton.style.cursor = "auto";
-  } else {
-// if the timer was already paused, when they click pause again, start the timer again
-startTimer();
-  }
-}
-function resetTimer(){
-  clearInterval(tInterval);
-  savedTime = 0;
-  difference = 0;
-  paused = 0;
-  running = 0;
-  timerDisplay.innerHTML = 'Start Timer!';
-  timerDisplay.style.background = "#A90000";
-  timerDisplay.style.color = "#fff";
-  timerDisplay.style.cursor = "pointer";
-  startTimerButton.classList.remove('lighter');
-  pauseTimerButton.classList.remove('lighter');
-  startTimerButton.style.cursor = "pointer";
-  pauseTimerButton.style.cursor = "auto";
-}
-function getShowTime(){
-  updatedTime = new Date().getTime();
-  if (savedTime){
-    difference = (updatedTime - startTime) + savedTime;
-  } else {
-    difference =  updatedTime - startTime;
-  }
-  // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
-hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
-  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
-}
+document.getElementById("focussesh").addEventListener("click", function(){ countdown(25)}); // MAKE SURE THIS ID CORRESPONDS TO THE BUTTON ID IN HTML
+document.getElementById("shortbreak").addEventListener("click", function(){ countdown(5)}); // SAME AS ABOVE
+document.getElementById("longbreak").addEventListener("click", function(){ countdown(15)}); // SAME AS ABOVE
 
-getShowTime();
+// MAKE SURE THE "timeDisplay" ID is given to the tag that will contain the countdown time
+
+function countdown(inverval){
+  // setting or pausing the timer
+  if (seshOn == 1 || sBreakOn == 1 || lBreakOn == 1 ){
+    // pause the counter
+    document.getElementById("timerDisplay").innerHTML = Date(countDownTime)
+    timerOn = 0;
+
+  } else {
+    if (!((interval == 25 && prevState == 0) || (interval == 5 && prevState == 1) || (interval == 15 && prevState == 2))) { // if the current state is not the same and the previous
+      // restart the counter using interval as the set time
+      var now = new Date().getTime();
+      countDownTime = new Date(now.getTime+ interval*60000); 
+      timerOn = 1;
+    }
+
+    // update the countdown every second
+    if (timerOn == 1) {
+      var x = setInterval(function() {
+        now = Date.getTime();
+        var distance = countDownTime - now;
+
+        // convert the distance to hours, minutes, etc
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // display the result in the element with id "timerDisplay"
+        document.getElementById("timerDisplay").innerHTML = minutes + ":" + seconds;
+
+        if (distance < 0){
+          clearInterval(x);
+          document.getElementById("timerDisplay").innerHTML = "00:00"
+        }
+
+      }, 1000);
+
+    }
+
+  }
+
+}
