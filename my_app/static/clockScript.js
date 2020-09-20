018d2d120d9c3116ea34024c4574f6af0d570ef3
Hailey Dennis <haileydennis@aggiemail.usu.edu>
2020-09-20 00:54:28 -0600

    fullscreen html/css added

--- my_app/static/clockScript.js
+++ my_app/static/clockScript.js
@@ -10,15 +10,44 @@ function showTime() {
     hour -= 12;
     amPm = "PM"
     }
-    else {
+    if (hour == 0) {
+    hour = 12;
     amPm = "AM"
     }
 
-    let currentTime = hour + ":" + min + amPm
+    min = min < 10 ? "0" + min : min;
+
+    let currentTime = hour + ":" + min
+
 
 
 
     document.getElementById("clock").innerHTML = currentTime;
 }
 
+function showAMPM() {
+let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+
+    amPm = "AM";
+
+    if (hour > 12) {
+    hour -= 12;
+    amPm = "PM"
+    }
+    if (hour == 0) {
+    hour = 12;
+    amPm = "AM"
+    }
+
+    let currentAMPM = amPm
+
+
+    document.getElementById("AMPM").innerHTML = currentAMPM;
+
+}
+
+
 showTime();
+showAMPM();
